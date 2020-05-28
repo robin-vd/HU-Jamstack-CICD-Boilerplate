fc2dfb25aecec8b2b19cfe4684a7da4fd7943c4e
Robin.vd <robin,vandalen@student.hu.nl>
2020-05-28 10:54:54 +0200

    add client const

--- src/scripts/sourceData.js
+++ src/scripts/sourceData.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 const contentful = require('contentful');
 
 const client = contentful.createClient({
-    space: '', // Insert Contentful Space ID
-    accessToken: '' // Insert Contentful Content Delivery API access token
+    space: 'f3jlp8tgx3mt', // Insert Contentful Space ID
+    accessToken: '6_UNlNp7Laq0OC2qcYb5EEwCvCYeM0P7rV_WnX0UtGw' // Insert Contentful Content Delivery API access token
 });
 
 client.getEntries({
